873d9a26bb019f4bc1992ef7467a4fa8a4761a7d
12312312312 <vladtsibizov@yandex.ru>
2019-04-20 02:11:49 +0300

    macros

---
+++ macros/a.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <chrono>
+
+#define TIME_TEST(func) \
+            TimeTest(func, #func)
+#define PRINT(x) cout << #x
+
+using namespace std;
+
+void Foo1(){
+    for(int i = 0; i < 100000; ++i){
+        cout << i << ' ';
+    }
+    cout << endl;
+}
+
+void Foo2(){
+    for(int i = 0; i < 1000000; ++i){
+        cout << i << ' ';
+    }
+    cout << endl;
+}
+
+template <class T>
+void TimeTest(T function, const string& func_name){
+    auto startTime = chrono::steady_clock::now();
+    function();
+    auto stopTime = chrono::steady_clock::now();
+    int ms = chrono::duration_cast<chrono::milliseconds>
+        (stopTime - startTime).count();
+    cout << func_name << " time: " << ms << " ms." << endl;
+}
+
+void PrintHello(){
+    cout << "hello\n";
+}
+
+int main(){
+
+    TIME_TEST(Foo1);
+    TIME_TEST(Foo2);
+    TIME_TEST(PrintHello);
+    PRINT(hello world);
+
+
+    return 0;
+}
