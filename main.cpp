2e8d124fd9886cc4d2c67b6b3daac094a0e4a064
12312312312 <vladtsibizov@yandex.ru>
2019-04-21 23:06:47 +0300

    object iteration

--- main.cpp
+++ main.cpp
@@ -1,59 +1,36 @@
-#include <string>
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-#define AS_KV(x) #x << " = " << x
+template<class Iterator>
+struct IteratorRange{
+    Iterator first, last;
 
-template<class T>
-class Vec{
-public:
-    Vec(){}
-    Vec(const vector<T>& v){
-        assign(v);
+    Iterator begin(){
+        return first;
     }
-    T const& At(size_t i) const {
-        return v_.at(i);
+    Iterator end(){
+        return last;
     }
-    T Back() const {
-        return v_.back();
-    }
-    void assign(const vector<T>& v){
-        v_ = v;
-    }
-    size_t Size() const {
-        return v_.size();
-    }
-    bool Empty() const {
-        return v_.empty();
-    }
-private:
-    vector<T> v_;
 };
 
 template<class T>
-ostream& operator<<(ostream& stream, const Vec<T>& v){
-    stream << "{";
-    for(size_t i = 0; i + 1 < v.Size(); ++i){
-        stream << v.At(i) << ", ";
-    }
-    if (!v.Empty()){
-        stream << v.Back();
-    }
-    stream << "}";
-    return stream;
+IteratorRange<class vector<T>::iterator> Head(
+    vector<T>& v, size_t top
+){
+    return {
+        v.begin(), next(v.begin(), min(top, v.size()))
+    };
 }
 
-int main() {
-    Vec<int> v1({1, 2, 3, 4, 5});
-    Vec<string> v2({
-        "first",
-        "second",
-        "third"
-    });
-    cout << v1 << endl;
-    cout << v2 << endl;
-
+int main(){
+    vector<int> v = {1, 2, 3, 4, 5};
+    for(int& x : Head(v, 3)){
+        ++x;
+    }
+    for(int x : v){
+        cout << x << ' ';
+    }
     return 0;
 }
